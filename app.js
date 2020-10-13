3f3319a1eb06d4b185c629f43fb18ff1c9cae970
BerDuc <bernard_ducharme@yahoo.ca>
2020-10-13 00:05:46 -0400

    état de la première itération

--- app.js
+++ app.js
@@ -7,29 +7,14 @@ class App extends React.Component {
             tempsPause: 5,
             tempsTravail: 25
         }
-        this.handleChangePause = this.handleChangePause.bind(this);
-        this.handleChangeTravail = this.handleChangeTravail.bind(this); 
+       
     }
 
-    handleChangePause(temps){
-        console.log("handleChangePause app")
-        this.setState({
-            tempsPause: temps
-        });
-    }
-
-    handleChangeTravail(temps){
-        console.log("handleChangeTravail app"+temps)
-        this.setState({
-            tempsTravail: temps
-        });
-        console.log(this.state.tempsTravail);
-    }
+  
 
     render(){
         return <div>
             <Pomodoro tempsTravail={this.state.tempsTravail} tempsPause={this.state.tempsPause}/>
-            <EntreeTemps handleChangePause={this.handleChangePause} handleChangeTravail={this.handleChangeTravail}/>
         </div>
     }
 }
