e5cf7cb239d1b2d978f156d7d88338a37ae8f2a4
BerDuc <bernard_ducharme@yahoo.ca>
2020-10-13 15:04:49 -0400

    entrée du temps

--- entreeTemps.jsx
+++ entreeTemps.jsx
@@ -2,8 +2,8 @@ class EntreeTemps extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            tempsPause: 0,
-            tempsTravail: 0
+            tempsPause: this.props.tempsPause,
+            tempsTravail: this.props.tempsTravail
         }
         this.handleChangePause = this.handleChangePause.bind(this);
         this.handleChangeTravail = this.handleChangeTravail.bind(this); 
@@ -12,23 +12,34 @@ class EntreeTemps extends React.Component {
     handleChangePause(e){
         this.setState({
             tempsPause: e.target.value
+        }, () => {this.props.handleChangePause(this.state.tempsPause)
         });
-        this.props.handleChangePause(this.state.tempsPause); 
     }
+    
+         
+    
+
     handleChangeTravail(e){
         this.setState({
             tempsTravail: e.target.value
-        })
-        console.log("state travail: "+this.state.tempsTravail)
-        this.props.handleChangeTravail(this.state.tempsTravail); 
+        }, () => {this.props.handleChangeTravail(this.state.tempsTravail)
+        });
+        
     }
 
+    
+    
+    
     render(){
-        return <div>
-            <label>Temps pour les périodes de travail</label>
-            <input type="text" value={this.state.tempsTravail} onChange={this.handleChangeTravail} />
-            <label>Temps pour les périodes de pause</label>
-            <input type="text" value={this.state.tempsPause} onChange={this.handleChangePause} />
+        return <div className="container">
+            <div>
+                <label className="label">Temps pour les périodes de travail</label>
+                <input type="text" className="input" value={this.state.tempsTravail} onChange={this.handleChangeTravail} />
+            </div>
+            <div>
+                <label className="label">Temps pour les périodes de pause</label>
+                <input type="text" className="input" value={this.state.tempsPause} onChange={this.handleChangePause} />
+            </div>
         </div>
     }
 }
