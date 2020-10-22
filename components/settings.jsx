c15840ed73515cd02bf74e947c36baeabe4c070f
BerDuc <bernard_ducharme@yahoo.ca>
2020-10-22 00:49:52 -0400

    simplification du composant. C'était une classe, c'est maintenant une fonction

--- components/settings.jsx
+++ components/settings.jsx
@@ -1,58 +1,29 @@
-class Settings extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            tempsPause: this.props.tempsPause,
-            tempsTravail: this.props.tempsTravail,
-            chronoAuto: false
-        }
-        this.alarmes = alarmes; 
-        this.handleChangePause = this.handleChangePause.bind(this);
-        this.handleChangeTravail = this.handleChangeTravail.bind(this); 
-        this.handleChangeChrono = this.handleChangeChrono.bind(this); 
-    }
-
-    handleChangePause(e){
-        this.setState({
-            tempsPause: e.target.value
-        }, () => {this.props.handleChangePause(this.state.tempsPause)
-        });
-    }
-
-    handleChangeTravail(e){
-        this.setState({
-            tempsTravail: e.target.value
-        }, () => {this.props.handleChangeTravail(this.state.tempsTravail)
-        });        
-    }
-
-    handleChangeChrono(e){
-        this.setState({
-            chronoAuto: e.target.checked
-        }, () => {this.props.handleChangeChrono(this.state.chronoAuto)
-        });
-    }
-    
-    render(){
+function Settings({ 
+    tempsTravail, 
+    tempsPause, 
+    handleChangeTravail, 
+    handleChangePause, 
+    handleChangeChrono, 
+    handleChangeAlarm
+}){     
         return <div className="section">
             <div>
                 <label className="label">Temps pour les périodes de travail</label>
-                <input type="text" className="input" value={this.state.tempsTravail} onChange={this.handleChangeTravail} />
+                <input type="text" className="input" value={tempsTravail} onChange={handleChangeTravail} />
             </div>
             <div>
                 <label className="label">Temps pour les périodes de pause</label>
-                <input type="text" className="input" value={this.state.tempsPause} onChange={this.handleChangePause} />
+                <input type="text" className="input" value={tempsPause} onChange={handleChangePause} />
             </div>
             <div>
                 <label className="label">Démarrer automatiquement le chrono suivant  
-                    <input className="checkbox" type="checkbox" checked={this.state.chronoAuto} onChange={this.handleChangeChrono}/>
+                    <input className="checkbox" type="checkbox" onChange={handleChangeChrono}/>
                 </label>
             </div>
             <label className="label">Choisir sa sonnerie</label>
             <SelecteurSons 
-                alarmes={this.alarmes}
-                handleChange={this.props.handleChangeAlarm}
+                alarmes={alarmes}
+                handleChange={handleChangeAlarm}
             /> 
-        </div>
-    }
+        </div>    
 }
